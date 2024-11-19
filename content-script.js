af53b1a22ad6b404795b24aa01de65027e1fa280
HussainElemam <130051441+HussainElemam@users.noreply.github.com>
2024-11-19 18:02:42 +0400

    add all files

---
+++ content-script.js
@@ -0,0 +1,7 @@
+const loginForm = document.getElementById("uaeuLoginForm");
+const altOption = document.getElementById("alternateLoginOptions");
+const altOptionBack = document.getElementById("alternateLoginOptionsBack");
+
+loginForm.style.display = "block";
+altOptionBack.style.display = "block";
+altOption.style.display = "none";
